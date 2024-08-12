1579306d1289138e4d2c85d8c12d8af9ec9f6465
Worlako Dzokoto <worlaco.dzo@gmail.com>
2024-08-12 00:47:30 +0000

    Added 1-concat

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def add(a: float, b: float) -> float:
     """Add two floats and return the result."""
     return a + b
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 def concat(str1: str, str2: str) -> str:
-    """Concatenate two strings and return the result."""
+    """Add two strings and return the result."""
     return str1 + str2
